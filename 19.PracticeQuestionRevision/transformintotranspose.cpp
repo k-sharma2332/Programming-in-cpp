96f9766fe52f2a3683be651d84643ee4a6273b9e
Khushi Sharma <ls390010@gmail.com>
2026-01-02 18:26:43 +0530

    2d-array practice questions added

---
+++ 19.PracticeQuestionRevision/transformintotranspose.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m; 
+    cout<<"Enter the number of rows: ";
+    cin>>m;
+    
+    int arr[m][m];
+    cout<<"Enter the elements of the array: "<<endl;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"\n";
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=0 ;i<m;i++){
+     for(int j=i+1;j<m;j++){
+        // swaping of i,j and j,i
+        int temp=arr[i][j];
+        arr[i][j]=arr[j][i];
+        arr[j][i]=temp;
+     }
+     cout<<endl;
+    }
+    
+
+   
+   // printing transpose
+    for(int i=0;i<m;i++){
+     for(int j=0;j<m;j++){
+          cout<<arr[i][j]<<" ";
+     }
+     cout<<endl;
+    }
+}
